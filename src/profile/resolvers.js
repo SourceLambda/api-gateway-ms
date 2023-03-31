92a6c768009f7f7a9dceff769415da76f48c68ce
Felipe Bedoya <jubedoyag@unal.edu.co>
2023-03-30 20:53:05 -0500

    pull from origin/profile

---
+++ src/profile/resolvers.js
@@ -0,0 +1,35 @@
+import { generalRequest } from '../utilities';
+import { url, port, entryPoint } from './server';
+
+const URL = `http://${url}:${port}/${entryPoint}`;
+
+const resolvers = {
+	Query: {
+		profileById: (_, { id }) =>
+			generalRequest(`${URL}/profile/${id}`, 'GET'),
+
+		cardsByProfileId: (_, { id_profile }) =>
+			generalRequest(`${URL}/profile/${id_profile}/cards`, 'GET'),
+
+		addressessByProfileId: (_, { id_profile }) =>
+			generalRequest(`${URL}/profile/${id_profile}/addresses`, 'GET'),
+	},
+	Mutation: {
+		//create
+		createProfile: (_, { profile }) =>
+			generalRequest(`${URL}/profile`, 'POST', profile),
+
+		createCardToProfile: (_, { id_profile, card }) =>
+			generalRequest(`${URL}/profile/${id_profile}/card`, 'POST', card),
+
+		createAddressToProfile: (_, { id_profile, address }) =>
+			generalRequest(`${URL}/profile/${id_profile}/address`, 'POST', address),
+
+/* 		updateCategory: (_, { id, category }) =>
+			generalRequest(`${URL}/${id}`, 'PUT', category),
+		deleteCategory: (_, { id }) =>
+			generalRequest(`${URL}/${id}`, 'DELETE') */
+	}
+};
+
+export default resolvers;
---
+++ src/profile/server.js
@@ -0,0 +1,3 @@
+export const url = 'localhost' //no olvidar cambiar a host.docker.internal cuando se despliegue en docker
+export const port = '9090'
+export const entryPoint = 'api'
